a920009def9cd9697f74b4249fd5c30f178cd12c
tnirms <rawish49@gmail.com>
2025-04-08 12:54:33 -0700

    version 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,47 @@
-
+import { useState } from "react";
+import Die from "./Die";
+import { nanoid } from "nanoid"
 
 function App() {
+
+    const [dice, setDice] = useState(() => generateAllNewDice())
+
+    const diceElements = dice.map(dieObj => 
+        <Die 
+        key={dieObj.id}
+        value={dieObj.value}
+        isHeld={dieObj.isHeld}
+        id={dieObj.id} />
+    )
+
+
+    function generateAllNewDice() {
+        const newDice = []
+        for (let i=0; i<10; i++) {
+            const randomNum = {
+                value: Math.floor(Math.random() * 6 + 1),
+                isHeld: false,
+                id:nanoid()
+        }
+        newDice.push(randomNum)
+    }
+        return newDice
+    }
+
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+
+
     return (
         <>
         <main>
             <h1>Tenzies</h1>
             <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
         </>
     ) 
@@ -16,11 +52,6 @@ export default App
 
 
 //Steps:
-//2. Add empty buttons manually 
-//3. Function to generate a random number. Create an array of 10 random numbers (from 1-6)
-//4. Move those 10 random numbers to the page (from memory) using State (used map to assign the number to each button)
-//5. Re-roll all 10 dice
-//6. Change dice to objects so die can eventually hold the dice (added to generateAllNewDice)
 //7. Add conditional styling to the Die component so that when its held, its background color changes 
 //8. Make it so that the die selection is held. Create a function 'hold' that takes 'id' as a parameter
 //9. Update the isHeld property to true on the correct die in an array
